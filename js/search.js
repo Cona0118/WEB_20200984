17fb829f37fcf3d7af4dc37cf61394a9a7c5afc3
Cona <chpark0101@gmail.com>
2023-03-29 05:31:02 +0000

    웹프로그래밍 4주차

---
+++ js/search.js
@@ -0,0 +1,11 @@
+document.getElementById("search_btn").addEventListener('click', search_message);
+/*
+document : 최상위 문서 
+getElementById : 문서의 특정 id를 가져온다 
+addEventListener : 이벤트 등록
+click : 클릭(이벤트)
+search_message : 함수
+*/
+function search_message(){
+	alert("검색을 수행합니다!");
+}
