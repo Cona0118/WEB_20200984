981d5f26bd915f1683947cdec5c66de2534ff330
Cona <chpark0101@gmail.com>
2023-05-03 05:19:51 +0000

    9주차 웹프

--- js/login.js
+++ js/login.js
@@ -5,19 +5,28 @@ function login(){
     
     form.action = "../index_login.html";
     form.method = "get";
+	
+	let id_test = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$/.test(id);
+	let pass_test = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/.test(password);
     
-    if(id.value.length === 0 || password.value.length === 0){
-        alert("아이디와 비밀번호를 모두 입력해주세요.");
-    }else{
-        form.submit();
-    }
+	if(id_test == false || pass_test == false){
+		alert(id_test+pass_test);
+	}
+	else{
+    	if(id.value.length === 0 || password.value.length === 0){
+        	alert("아이디와 비밀번호를 모두 입력해주세요.");
+    	}
+		else{
+        	form.submit();
+	}
+	}
 }
 
 function get_id(){
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
     var url = location.href; // 현재 접속 중인 주소 정보 저장
-    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('%'); // ?기준 slice 한 후 split 으로 나눔
         for(var i = 0; i < parameters.length; i++) { 
 		    var varName = parameters[i].split('=')[0];
             if (varName.toUpperCase() == paramName.toUpperCase()) {
@@ -26,6 +35,8 @@ function get_id(){
             // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
 		    }
 	    } // 2중 for문 끝
-}; // 함수 끝
-alert(getParameters('id') + '님 방갑습니다!'); // 메시지 창 출력
+} // 함수 끝
+alert(getParameters('id') + '님 반갑습니다!'); // 메시지 창 출력
 }
+
+
